8ef9c70053b05464d051441bc7f84806f5168ff5
Wilbert 張升耀 <100257271+Bertywertyy@users.noreply.github.com>
2024-09-25 20:40:03 +0700

    Update streamlitwebsite.py

--- streamlitwebsite.py
+++ streamlitwebsite.py
@@ -148,6 +148,7 @@ st.write("---")
 st.write("🚀", "**codecademy.com | Python 2**")
 st.write("2022 October - December")
 st.image("https://i.im.ge/2024/03/17/RPZW5D.WhatsApp-Image-2024-02-21-at-22-02-13-7308a444-3.jpeg", width=400)
+st.markdown('[PowerPoint](https://docs.google.com/presentation/d/1gO3TiF6HdYTqyM5ak7bD_DiMCHI0IC4C3h1sLWPnN58/edit?usp=sharing)')
 st.write(
     """
 - ► Embarking on my coding journey, I chose Codecademy for its free lessons and Python for its beginner-friendly language. 
